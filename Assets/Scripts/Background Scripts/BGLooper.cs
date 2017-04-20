83a372903008baf320c886c3d3edc71c0b9365bd
Vítor Luís <vitorluis.mauricio@hotmail.com>
2017-04-20 04:55:43 -0300

    Background
    Background and ground scaler and looper created

---
+++ Assets/Scripts/Background Scripts/BGLooper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGLooper : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 0.1f;
+    Vector2 offset;
+
+    Material material;
+
+    void Awake() {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update() {
+        offset = material.GetTextureOffset("_MainTex");
+        offset.x += speed * Time.deltaTime;
+        material.SetTextureOffset("_MainTex", offset);
+    }
+}
---
+++ Assets/Scripts/Background Scripts/BGScaler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour {
+
+	void Awake() {
+        float height = Camera.main.orthographicSize * 2f;
+        float width = height * Screen.width / Screen.height;
+
+        if(name == "Background") {
+            transform.localScale = new Vector3(width, height, 0);
+        }
+        else {
+            transform.localScale = new Vector3(width + 3, height / 2, 0);
+        }
+        
+    }
+}
